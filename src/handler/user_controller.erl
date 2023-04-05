aac8450f155f8e2f0a2573fd547613084413b758
zhengweixing <529459515@qq.com>
2023-04-06 01:28:45 +0800

    Add log

--- src/handler/user_controller.erl
+++ src/handler/user_controller.erl
@@ -37,8 +37,9 @@ do_request(post_logout, Args, Context, _Req) ->
 
 do_request(get_login, Args, Context, Req) ->
     do_request(post_login, Args, Context, Req);
-do_request(post_login, #{<<"username">> := UserName, <<"password">> := Password}, _Context, _Req) ->
-    case ehttpd_hook:run('user.login', [UserName, Password], #{}) of
+do_request(post_login, #{<<"username">> := UserName, <<"password">> := Password}, _Context, Req) ->
+    Log = ehttpd_utils:get_log(Req),
+    case ehttpd_hook:run('user.login', [UserName, Password, Log], #{}) of
         {ok, Result} ->
             {200, Result};
         {error, Result} ->
