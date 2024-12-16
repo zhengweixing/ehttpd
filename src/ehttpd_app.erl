2188119cc453e33ff658300e94dd4c6140d409af
zhengweixing <529459515@qq.com>
2024-12-16 23:22:13 +0800

    Rm Controller

--- src/ehttpd_app.erl
+++ src/ehttpd_app.erl
@@ -12,20 +12,9 @@
 %% Application callbacks
 %% ===================================================================
 start(_StartType, _StartArgs) ->
-    {ok, Sup} = ehttpd_sup:start_link(),
-    Port = application:get_env(ehttpd, port, 6080),
-    {ok, _} = ehttpd_server:start(default, Port, get_env()),
-    {ok, Sup}.
+    ehttpd_sup:start_link().
 
 stop(_State) ->
     ok.
 
 
-get_env() ->
-    lists:foldl(
-        fun(Key, Acc) ->
-            case application:get_env(ehttpd, Key, undefined) of
-                undefined -> Acc;
-                Value -> Acc#{Key => Value}
-            end
-        end, #{}, [swagger, expire, debug, docroot, cacertfile, certfile, keyfile, access_control_allow_headers]).
